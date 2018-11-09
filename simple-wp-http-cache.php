1816dbe82b00a3a6b76c6ae686c90d689eae8519
Edwin Cromley <cromleyedwin89@gmail.com>
2018-11-08 23:44:32 -0500

    Basic wp scaffold set up.

---
+++ simple-wp-http-cache.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Simple Wp Http Cache
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     simple-wp-http-cache
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Simple_Wp_Http_Cache
+ */
+
+// Your code starts here.
